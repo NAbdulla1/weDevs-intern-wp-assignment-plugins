207584f5f00fafa8b8fb8fbf858969a75d2b1573
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-07 17:10:27 +0600

    add emphasise hook for view count in Posts View plugin

--- Assignment02_Posts_View/includes/Frontend/View_Counter.php
+++ Assignment02_Posts_View/includes/Frontend/View_Counter.php
@@ -3,10 +3,18 @@
 
 namespace A02_Posts_View_Counter\Frontend;
 
-
+/**
+ * Class View_Counter
+ * @package A02_Posts_View_Counter\Frontend
+ */
 class View_Counter {
+
+	/**
+	 * View_Counter constructor.
+	 */
 	public function __construct() {
-		add_filter( 'the_content', [ $this, 'add_view_count' ], 1000, 1 );
+		add_filter( 'the_content', [ $this, 'add_view_count' ], 10, 1 );
+		add_filter( 'a02_posts_view_view_counter_hook', [ $this, 'emphasise_count_value' ], 10, 1 );
 	}
 
 	/**
@@ -21,6 +29,8 @@ class View_Counter {
 			$postId = get_the_ID();
 			$this->init_view_count( $postId );
 			$count = $this->update_and_get_view_count( $postId );
+
+			$count = apply_filters( 'a02_posts_view_view_counter_hook', $count );
 
 			return $content . "<p>Total View: $count</p>";
 		}
@@ -52,5 +62,14 @@ class View_Counter {
 		update_post_meta( $postId, 'view_count', $count );
 
 		return $count;
+	}
+
+	/**
+	 * @param $count
+	 *
+	 * @return string
+	 */
+	public function emphasise_count_value( $count ): string {
+		return "<em>$count</em>";
 	}
 }
