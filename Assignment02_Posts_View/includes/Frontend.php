49e153c174294113648ec2a5c7a79f7f43ff50b4
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-11 14:40:00 +0600

    update Posts View Plugin as mentioned in 'Day 14 - 15' assignment

--- Assignment02_Posts_View/includes/Frontend.php
+++ Assignment02_Posts_View/includes/Frontend.php
@@ -4,11 +4,13 @@
 namespace A02_Posts_View_Counter;
 
 
+use A02_Posts_View_Counter\Frontend\Post_View_Filter;
 use A02_Posts_View_Counter\Frontend\View_Counter;
 
 class Frontend {
 
 	public function __construct() {
 		new View_Counter();
+		new Post_View_Filter();
 	}
 }
