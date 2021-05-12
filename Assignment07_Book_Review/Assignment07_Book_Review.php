a64c210e5af72acdbf1e90346e3c15245b4bec8b
Md. Abdulla Al Mamun <nayonabdulla@gmail.com>
2021-05-12 16:50:25 +0600

    create Book Review Plugin

---
+++ Assignment07_Book_Review/Assignment07_Book_Review.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Name: Assignment07 Book Review
+ * Description: A plugin to work with books and reviews
+ * Plugin URI: https://github.com/NAbdulla1/weDevs-intern-wp-assignment-plugins/tree/main/Assignment07_Book_Review
+ * Author: Md. Abdulla Al Mamun
+ * Author URI: https://github.com/NAbdulla1
+ * Version: 1.0
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+class Assignment07_Book_Review {
+
+	const version = '1.0';
+
+	private function __construct() {
+		$this->define_constants();
+
+		register_activation_hook( __FILE__, [ $this, 'activate' ] );
+
+		add_action( 'plugin_loaded', [ $this, 'init_plugin' ] );
+	}
+
+	public static function init() {
+		static $instance = false;
+		if ( ! $instance ) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	private function define_constants() {
+		define( "A07_BOOK_REVIEW_VERSION", self::version );
+		define( "A07_BOOK_REVIEW_FILE", __FILE__ );
+		define( "A07_BOOK_REVIEW_PATH", __DIR__ );
+		define( "A07_BOOK_REVIEW_URL", plugins_url( '', A07_BOOK_REVIEW_FILE ) );
+		define( "A07_BOOK_REVIEW_ASSETS", A07_BOOK_REVIEW_URL . '/assets' );
+	}
+
+	public function activate() {
+		$installed = get_option( 'a07_book_review_installed', false );
+		if ( ! $installed ) {
+			update_option( 'a07_book_review_installed', time() );
+		}
+		update_option( 'a07_book_review_version', A07_BOOK_REVIEW_VERSION );
+	}
+
+	public function init_plugin() {
+		new \A07_Book_Review\Books_CPT();
+	}
+}
+
+function a07_book_review() {
+	Assignment07_Book_Review::init();
+}
+
+a07_book_review();
---
+++ Assignment07_Book_Review/includes/Books_CPT.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace A07_Book_Review;
+
+
+class Books_CPT {
+	public function __construct() {
+		add_action( 'init', [ $this, 'register_book_custom_post_type' ] );
+	}
+
+	public function register_book_custom_post_type() {
+		$labels = array(
+			'name'               => _x( 'Books', 'post type general name' ),
+			'singular_name'      => _x( 'Book', 'post type singular name' ),
+			'add_new'            => _x( 'Add New', 'book' ),
+			'add_new_item'       => __( 'Add New Book' ),
+			'edit_item'          => __( 'Edit Book' ),
+			'new_item'           => __( 'New Book' ),
+			'all_items'          => __( 'All Books' ),
+			'view_item'          => __( 'View Book' ),
+			'search_items'       => __( 'Search Books' ),
+			'not_found'          => __( 'No books found' ),
+			'not_found_in_trash' => __( 'No books found in the Trash' ),
+			'menu_name'          => 'Books'
+		);
+		$args   = array(
+			'labels'        => $labels,
+			'description'   => 'Holds our books and book specific data',
+			'public'        => true,
+			'has_archive'   => true,
+		);
+		register_post_type( 'a07_book_review_book', $args );
+	}
+}
